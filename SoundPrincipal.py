94e4431a8d6dae694caad9dd2e33ca7b5091a4a1
cocgabriel <gabriel.cochet@ynov.com>
2025-10-09 15:42:04 +0200

    boutons son et parametres

--- SoundPrincipal.py
+++ SoundPrincipal.py
@@ -20,9 +20,9 @@ def stop_menu_music():
 
 def play_crash_sound():
     pygame.mixer.music.stop()  # Arrête la musique en cours
-    crash_sound = pygame.mixer.Sound(r"C:\Users\rodne\Documents\Projet-PyGame\assets\Sound\Sound effect\car-crash-sound.mp3")
+    crash_sound = pygame.mixer.Sound(r"assets\Sound\Sound effect\car-crash-sound.mp3")
     crash_sound.play()
 
 def play_boing_sound():
-    boing = pygame.mixer.Sound(r"C:\Users\rodne\Documents\Projet-PyGame\assets\Sound\Sound effect\boing.mp3")
+    boing = pygame.mixer.Sound(r"assets\Sound\Sound effect\boing.mp3")
     boing.play()
